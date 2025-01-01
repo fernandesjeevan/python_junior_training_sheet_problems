1e805740b91fdef34c91f9602f23eb777ef666cb
jeevan-fernandes <jeevanfernandes111@gmail.com>
2025-01-01 16:08:26 +0000

    feat -boy or girl in one line

--- boy_or_girl.py
+++ boy_or_girl.py
@@ -1,10 +1,16 @@
-string = input()
-unique_string = ""
-for s in string:
-    if s not in unique_string:
-        unique_string+= s
-if len(unique_string)%2 ==0:
-    print("CHAT WITH HER!")
+# string = input()
+# string = list(string)
+# print(string)
+# unique_string = set(string)
+
+# # for s in string:
+# #     if s not in unique_string:
+# #         unique_string+= s
+# if len(unique_string)%2 ==0:
+#     print("CHAT WITH HER!")
  
-else:
-    print("IGNORE HIM!")
+# else:
+#     print("IGNORE HIM!")
+
+
+print("CHAT WITH HER!") if len(set(input()))%2 == 0 else print("IGNORE HIM!")
